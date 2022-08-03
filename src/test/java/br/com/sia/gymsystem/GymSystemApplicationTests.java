9ecc6974b5ddfb9f6c3b346881495001e676f113
igor.fernandes <igor.fernandes@foursys.com.br>
2022-08-03 09:26:19 -0300

    Inicialização do projeto.

---
+++ src/test/java/br/com/sia/gymsystem/GymSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sia.gymsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
