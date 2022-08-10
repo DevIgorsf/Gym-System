8c1e2b21b6e949c38c3f73832893107e0b103ad4
igor.fernandes <igor.fernandes@foursys.com.br>
2022-08-09 21:37:52 -0300

    Atualizar dados cliente.

--- src/main/java/br/com/sia/gymsystem/controller/InstrutorController.java
+++ src/main/java/br/com/sia/gymsystem/controller/InstrutorController.java
@@ -1,15 +1,14 @@
 package br.com.sia.gymsystem.controller;
 
+import br.com.sia.gymsystem.dto.ClienteDto;
 import br.com.sia.gymsystem.dto.InstrutorDto;
+import br.com.sia.gymsystem.form.ClienteForm;
 import br.com.sia.gymsystem.form.InstrutorForm;
 import br.com.sia.gymsystem.service.InstrutorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -20,8 +19,18 @@ public class InstrutorController {
     @Autowired
     private InstrutorService instrutorService;
 
+    @GetMapping("/dados/{id}")
+    public ResponseEntity<InstrutorDto> getDados(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(instrutorService.getDados(id));
+    }
+
     @PostMapping("/cadastrar")
     public ResponseEntity<InstrutorDto> cadastarInstrutor(@RequestBody @Valid InstrutorForm form) {
         return ResponseEntity.status(HttpStatus.OK).body(instrutorService.cadastrarInstrutor(form));
+    }
+
+    @PutMapping("/dados/{id}")
+    public ResponseEntity<InstrutorDto> atualizarInstrutor(@PathVariable Long id, @RequestBody @Valid ClienteForm form) {
+        return ResponseEntity.status(HttpStatus.OK).body(instrutorService.atualizarInstrutor(id, form));
     }
 }
--- src/main/java/br/com/sia/gymsystem/service/InstrutorService.java
+++ src/main/java/br/com/sia/gymsystem/service/InstrutorService.java
@@ -2,6 +2,7 @@ package br.com.sia.gymsystem.service;
 
 import br.com.sia.gymsystem.dto.InstrutorDto;
 import br.com.sia.gymsystem.enums.RoleName;
+import br.com.sia.gymsystem.form.ClienteForm;
 import br.com.sia.gymsystem.form.InstrutorForm;
 import br.com.sia.gymsystem.model.*;
 import br.com.sia.gymsystem.repository.*;
@@ -61,5 +62,30 @@ public class InstrutorService {
         Instrutor instrutorSaved = instrutorRepository.save(instrutor);
 
         return modelMapper.map(instrutorSaved, InstrutorDto.class);
+    }
+
+    public InstrutorDto atualizarInstrutor(Long id, ClienteForm form) {
+        Optional<Instrutor> instrutor = instrutorRepository.findById(id);
+
+        if(instrutor.isEmpty()) {
+            throw new EntityNotFoundException("Cliente não encontrado");
+        }
+        Endereco endereco = instrutor.get().getEndereco();
+        if(!form.getEstado().equals(endereco.getEstado())) endereco.setEstado(form.getEstado());
+        if(!form.getCidade().equals(endereco.getCidade())) endereco.setCidade(form.getCidade());
+        if(!form.getBairro().equals(endereco.getBairro())) endereco.setBairro(form.getBairro());
+        if(!form.getRua().equals(endereco.getRua())) endereco.setRua(form.getRua());
+        if(form.getNumero() != endereco.getNumero()) endereco.setNumero(form.getNumero());
+        if(form.getComplemento() != endereco.getComplemento()) endereco.setComplemento(form.getComplemento());
+        if(!instrutor.get().getEndereco().equals(endereco)) enderecoRepository.save(endereco);
+
+        Usuario usuario = instrutor.get().getUsuario();
+        if(!form.getUsername().equals(usuario.getUsername())) usuario.setUsername(form.getUsername());
+        if(!form.getPassword().equals(usuario.getPassword())) usuario.setPassword(form.getPassword());
+        usuarioRepository.save(usuario);
+
+        Optional<Instrutor> instrutorSaved = instrutorRepository.findById(id);
+
+        return modelMapper.map(instrutorSaved.get(), InstrutorDto.class);
     }
 }
